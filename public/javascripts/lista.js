f1e3cb486673fc53eecbd3475c66f73cf5ebe1c9
Elena Sontu <ella_west@icloud.com>
2019-07-08 21:49:28 +0300

    added marks

--- public/javascripts/lista.js
+++ public/javascripts/lista.js
@@ -4,9 +4,11 @@ var editPersonId;
 var API_URL = {
     CREATE: '...',
     READ: 'users',
-    ADD:'users/add'
-
+    ADD:'users/add',
+    UPDATE : 'users/update',
+    DELETE : 'users/delete'
 };
+
 var API_METHOD = {
     CREATE: 'POST',
     READ: 'GET',
@@ -21,7 +23,8 @@ fetch(API_URL.READ).then(function (r) {
     display(persons);
 });
 
-const stars = document.querySelector('#rating')   
+const stars = document.querySelector('#rating');
+
 function display(persons) {
     var list = persons.map(function (person) {
         return `<tr>
@@ -30,23 +33,34 @@ function display(persons) {
             <td><input type="number" class="rate" value="0" max=5 name="${person.id}"></td>  
         </tr>`
     });
-
     document.querySelector('#lista tbody').innerHTML = list.join('');
 }
 
-const Students = {
-    getMarks: function () {
-        var marks = Array.from(document.querySelectorAll("input[type=number]"))
-        
-        return marks.map(input => {
-            return {
-                name: input.name,
-                nota: input.value
-            };
-        }).filter(mark => mark.nota > 0)
-    }
-}
+// const Students = {
+//     saveMarks: function() {
+//         //TODO READ signature 
+//         //READ Marks
+//         //POST MARKS
+//         var firstName = document.querySelector('[name=firstName]').value;
+//         var lastName = document.querySelector('[name=lastName]').value;
+//         var marks = document.querySelector('[name=marks]').value;
+//     },
 
+//     // inlineAdd: function(id, firstName, lastName, marks) {
+
+//     // },
+
+//     getMarks: function () {
+//         var marks = Array.from(document.querySelectorAll("input[type=number]"))
+        
+//         return marks.map(input => {
+//             return {
+//                 name: input.name,
+//                 nota: input.value
+//             };
+//         }).filter(mark => mark.nota > 0)
+//     }
+// };
 
 // if one parameter can skipp pharantesis 
 // "(value)" will be "value"
@@ -61,7 +75,6 @@ const search = value => {
 
 function initEvents() {
     const tbody = document.querySelector('#lista tbody')
-
 }
 
 initEvents();
