92f4fb29792d0c38f5f34eb8fd707e5f01c17617
Ana Sabau <ana.m.sabau@gmail.com>
2019-07-13 15:07:21 +0300

    afisarea tabelului 2

--- routes/users-db.js
+++ routes/users-db.js
@@ -20,6 +20,24 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.get('/ratings', function(req, res, next) {
+  pool.getConnection((err, connection) => {
+    const sql = `SELECT students.*, AVG(students_tests.grades) as avg FROM students LEFT JOIN students_tests ON (students.id = students_tests.student_id AND students_tests.test_id <> 3 ) GROUP BY students.id`;
+    connection.query(sql, (err, results) => {
+      
+      const sql = `SELECT students.id, AVG(students_tests.grades) as avg FROM students LEFT JOIN students_tests ON (students.id = students_tests.student_id AND students_tests.test_id = 3 ) GROUP BY students.id`;
+      connection.query(sql, (err, resultsRatings) => {
+        console.info(resultsRatings);
+        //var medie = results.map(function(result){
+         // result.rating = 
+       // })
+        res.json(results);
+        connection.release();
+      });
+    });
+  });
+});
+
 // http://localhost:3000/users/add
 router.post('/add', function(req, res, next) {
   var firstName = req.body.firstname;
